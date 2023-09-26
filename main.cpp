476a5e3d9ed7870d8b650c127cbd605c410b5a2b
Adam R. Scroggin <adam.scroggin@lftsoftware.co>
2023-09-26 09:36:20 -0400

    simple program that runs and compiles

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// C++ program that counts down to a certain data
+ 
+#include <iostream>
+using namespace std;
+ 
+///////////////////////////////////////////////////////
+//
+// main
+//
+///////////////////////////////////////////////////////
+int main()
+{
+    // Prints hello world
+    cout << "Hello World";
+ 
+    return 0;
+}
