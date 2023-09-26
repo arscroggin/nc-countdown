d20ce0dd4cc0d5b1dbf920b6544981cfff6a13c9
Adam R. Scroggin <adam.scroggin@lftsoftware.co>
2023-09-26 09:56:09 -0400

    not working yet

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nc-countdown
+
+To compile:
+clang++ -Wall -std=c++20 -stdlib=libc++ main.cpp
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 // C++ program that counts down to a certain data
  
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <format>
+
 using namespace std;
  
 ///////////////////////////////////////////////////////
@@ -11,7 +15,16 @@ using namespace std;
 int main()
 {
     // Prints hello world
-    cout << "Hello World";
+    cout << "Hello World\n";
+
+    auto now = std::chrono::system_clock::now();
+    std::string nowTimeString = std::format("{:%F %T}", now);
+
+    cout << nowTimeString;
  
     return 0;
 }
+
+
+
+
